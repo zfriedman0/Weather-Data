ec815d225f3625315a7740b34338c36be495a755
zfriedman0 <friedman1080@outlook.com>
2024-08-21 16:51:20 -0500

    Modified get_data to get location from user input.

--- get_data.py
+++ get_data.py
@@ -4,18 +4,22 @@ import psycopg2
 
 from dotenv import load_dotenv
 from datetime import datetime
+from geopy.geocoders import Nominatim
 
 load_dotenv()
 
 key = os.getenv("API_KEY")
-lat = os.getenv("LAT")
-lon = os.getenv("LON")
+geolocator = Nominatim(user_agent="weather-data")
+
+user_input = input("Enter a location: ")
+location = geolocator.geocode(user_input)
+print(f"Found {location.address}\nUsing {location.latitude}, {location.longitude}...")
+
+response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={location.latitude}&lon={location.longitude}&appid={key}&units=imperial")
+weather_data = response.json()
 
 conn = psycopg2.connect(f"dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASS')}")
 cur = conn.cursor()
-
-response = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={key}&units=imperial")
-weather_data = response.json()
 
 sunrise = datetime.fromtimestamp(weather_data['city']['sunrise']).strftime('%Y-%m-%d %H:%M:%S')
 sunset = datetime.fromtimestamp(weather_data['city']['sunset']).strftime('%Y-%m-%d %H:%M:%S')
